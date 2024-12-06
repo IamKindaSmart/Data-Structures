991d93faa0e78ebc6004896c2f26638434b61c87
IamKindaSmart <108837863+IamKindaSmart@users.noreply.github.com>
2024-12-06 20:59:41 +0000

    Binary Search Tree (BST).py

--- Binary Search Tree (BST).py
+++ Binary Search Tree (BST).py
@@ -1,16 +1,16 @@
 class Tree:
     def __init__(self,key):
         self.left=None
-        self.Right=None
-        self.data=key
+        self.right=None
+        self.val=key
 
 def insert(root, key): #key is the new value that we want to insert
     #empty tree causes the first node to become the root
     if root is None:
-        return Tree(root)
+        return Tree(key)
     
     #otherwise recur down the tree
-    if key < root.data: #if new value <  root
+    if key < root.val: #if new value <  root
         root.left = insert(root.left, key)
     
     else:
@@ -19,17 +19,32 @@ def insert(root, key): #key is the new value that we want to insert
     #return the root
     return root
 
-def Inorder(root):    #left - root - right
-    Inorder(root.left)
-    print(root.data, end='-')
-    Inorder(root.right)
+def search(root,key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left,key)
+    else:
+        return search(root.right,key)
+
+def Inorder(root):
+    if root:   #left - root - right
+        Inorder(root.left)
+        print(root.val, end='-')
+        Inorder(root.right)
 
 
-t = Tree(8)
-insert(t,6)
-insert(t,3)
-insert(t,8)
-insert(t,0)
-insert(t,1)
+t= None
+t= insert(t,6)
+t= insert(t,3)
+t= insert(t,8)
+t= insert(t,0)
+t= insert(t,1)
 
 Inorder(t)
+
+found_node = search(t,0)
+if found_node:
+    print("\n Element found")
+else:
+    print("\n Not found")
